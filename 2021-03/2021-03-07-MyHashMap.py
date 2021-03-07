036d6d21f192225e709a70f8fc32a4ba69544383
Xiyi Wang <xiyiwang116@outlook.com>
2021-03-07 09:35:34 -0500

    2021-03-07 challenge

---
+++ 2021-03/2021-03-07-MyHashMap.py
@@ -0,0 +1,39 @@
+"""
+LeetCode Challenge: Design HashMap (2021-03-07)
+
+Design a HashMap without using any built-in hash table libraries. 
+
+To be specific, your design should include these functions: 
+- put(key, value) : Insert a (key, value) pair into the HashMap. If the 
+  value already exists in the HashMap, update the value. 
+- get(key): Returns the value to which the specified key is mapped, or -1 
+  if this map contains no mapping for the key. 
+- remove(key) : Remove the mapping for the value key if this map contains 
+  the mapping for the key.
+
+Note: 
+- All keys and values will be in the range of [0, 1000000]. 
+- The number of operations will be in the range of [1, 10000].
+- Please do not use the built-in HashMap library.
+"""
+
+class MyHashMap:
+    def __init__(self):
+        self.map = {}
+        
+
+    def put(self, key: int, value: int) -> None:
+        # value will always be non-negative.
+        self.map[key] = value
+        
+
+    def get(self, key: int) -> int:
+        # Returns the value to which the specified key is mapped, 
+        # or -1 if this map contains no mapping for the key
+        return self.map[key] if key in self.map.keys() else -1
+        
+
+    def remove(self, key: int) -> None:
+        # Removes the mapping of the specified value key if this 
+        # map contains a mapping for the key
+        self.map.pop(key, "")
