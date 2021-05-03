fe1cf95a1e96202fe00cca3870f48b6e73248a71
Xiyi Wang <xiyiwang116@outlook.com>
2021-05-03 10:06:43 -0400

    numpy

---
+++ other-challenges/array-easy-maxProfit.py
@@ -0,0 +1,26 @@
+# array: easy - Best Time to Buy and Sell Stock II
+
+#   Say you have an array prices for which the ith 
+#   element is the price of a given stock on day i. 
+# 
+#   Design an algorithm to find the maximum profit. 
+#   You may complete as many transactions as you like 
+#   (i.e., buy one and sell one share of the stock 
+#   multiple times). 
+# 
+#   Note: You may not engage in multiple transactions 
+#   at the same time (i.e., you must sell the stock 
+#   before you buy again). 
+# 
+#   Constraints:
+#   * 1 <= prices.length <= 3 * 10^4 
+#   * 0 <= prices[i] <= 10^4
+
+def maxProfit(prices):
+    pass
+
+prices = [7,1,5,3,6,4] # 7
+# prices = [1,2,3,4,5] # 4
+# prices = [7,6,4,3,1] # 0
+
+print(maxProfit(prices))
